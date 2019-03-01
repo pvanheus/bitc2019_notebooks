358803fb5966a530f4cf0a5a5ec454396112e879
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-01 09:38:36 +0200

    my calculator Fri Mar  1 09:38:36 SAST 2019

--- code/my_calculator.py
+++ code/my_calculator.py
@@ -4,4 +4,8 @@ def arithmetic():
     if len(sys.argv) == 4:
         script = sys.argv[0]
         operation = sys.argv[1]
-        num1 = sys.
+        num1 = sys.argv[2]
+        num2 = sys.argv[3]
+        if operation == 'add':
+            print(num1, "+", num2, "=", int(num1) + int(num2))
+            
