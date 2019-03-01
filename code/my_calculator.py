d9321a42bbed52d9f56832816be7597504ee5312
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-01 10:12:36 +0200

    my calculator Fri Mar  1 10:12:36 SAST 2019

--- code/my_calculator.py
+++ code/my_calculator.py
@@ -3,7 +3,6 @@ import sys
 def arithmetic():
     # check that we have the correct number of parameters
     if len(sys.argv) == 4:   
-        script = sys.argv[0]
         operation = sys.argv[1]
         num1 = sys.argv[2]
         num2 = sys.argv[3]
@@ -21,5 +20,8 @@ def arithmetic():
                 sys.exit(1)
         else:
             print("operation not valid, only add and subtract operations are permitted")
+    else:
+        script = sys.argv[0]
+        print("Usage: " + )
             
             
